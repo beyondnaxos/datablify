61b81591e019f9e50df9bc5f4e481c681d856956
BeyondNaxos <adopteuntrip@gmail.com>
2023-05-08 13:25:22 +0200

    1.7 update empty search

--- index.jsx
+++ index.jsx
@@ -26,13 +26,13 @@ const Pagination = ({ nPages, currentPage, setCurrentPage }) => {
 
   const colorCurrentPage = (number) => {
     if (number === currentPage) {
-      return "black"
+      return 'black'
     }
   }
 
   const backgroundColorCurrentPage = (number) => {
     if (number === currentPage) {
-      return "white"
+      return 'white'
     }
   }
 
@@ -44,7 +44,11 @@ const Pagination = ({ nPages, currentPage, setCurrentPage }) => {
             key={1}
             className={styles.paginationButton}
             onClick={() => setCurrentPage(number)}
-            style={{ order: 0, color: colorCurrentPage(number), backgroundColor: backgroundColorCurrentPage(number) }}
+            style={{
+              order: 0,
+              color: colorCurrentPage(number),
+              backgroundColor: backgroundColorCurrentPage(number),
+            }}
           >
             {1}
           </button>
@@ -55,12 +59,20 @@ const Pagination = ({ nPages, currentPage, setCurrentPage }) => {
             key={1}
             className={styles.paginationButton}
             onClick={() => setCurrentPage(number)}
-            style={{ order: 0, color: colorCurrentPage(number), backgroundColor: backgroundColorCurrentPage(number) }}
+            style={{
+              order: 0,
+              color: colorCurrentPage(number),
+              backgroundColor: backgroundColorCurrentPage(number),
+            }}
           >
             {1}
           </button>
-          <button className={styles.paginationButton} style={{ order: 1 }} onClick={prevPage}>
-            <span className={styles.paginationButtonIcon}>{"<"}</span>
+          <button
+            className={styles.paginationButton}
+            style={{ order: 1 }}
+            onClick={prevPage}
+          >
+            <span className={styles.paginationButtonIcon}>{'<'}</span>
           </button>
         </>
       )}
@@ -68,7 +80,11 @@ const Pagination = ({ nPages, currentPage, setCurrentPage }) => {
         key={currentPage}
         className={styles.paginationButton}
         onClick={() => setCurrentPage(currentPage)}
-        style={{ order: 2, color: colorCurrentPage(currentPage), backgroundColor: backgroundColorCurrentPage(currentPage) }}
+        style={{
+          order: 2,
+          color: colorCurrentPage(currentPage),
+          backgroundColor: backgroundColorCurrentPage(currentPage),
+        }}
       >
         {currentPage}
       </button>
@@ -102,15 +118,21 @@ const Pagination = ({ nPages, currentPage, setCurrentPage }) => {
           >
             {pageNumbers.length}
           </button>
-          <button className={styles.paginationButton} style={{ order: pageNumbers.length + 1 }} onClick={nextPage}>
-            <span className={styles.paginationButtonIcon}>{">"}</span>
+          <button
+            className={styles.paginationButton}
+            style={{ order: pageNumbers.length + 1 }}
+            onClick={nextPage}
+          >
+            <span className={styles.paginationButtonIcon}>{'>'}</span>
           </button>
         </>
       )}
+      {/* <button className={styles.paginationButton} style={{ order: pageNumbers.length + 1 }} onClick={nextPage}>
+        <span className={styles.paginationButtonIcon}>{">"}</span>
+      </button> */}
     </div>
   )
 }
-
 
 export const Datablify = (props) => {
   const data = props.data
@@ -139,7 +161,7 @@ export const Datablify = (props) => {
   let timeOutId = null
 
   const nPages = Math.ceil(displayData.length / recordsPerPage)
-  const [sortTrigger, setSortTrigger] = React.useState(true);
+  const [sortTrigger, setSortTrigger] = React.useState(true)
 
   React.useEffect(() => {
     setCustomHeadColor(headColor)
@@ -152,16 +174,19 @@ export const Datablify = (props) => {
       const recordValuesString = recordValues.join(" ")
       return recordValuesString.toLowerCase().includes(searchTerm)
     })
-    setDisplayData(filteredData)
+    // if filtered data is emty , set displayData : message "no data found"
+    filteredData.length === 0 ? setDisplayData([{ "No data found": "no data found" }]) : setDisplayData(filteredData)
+
+    // setDisplayData(filteredData)
     setCurrentPage(1)
-    setSortTrigger((prev) => !prev);
+    setSortTrigger((prev) => !prev)
   }, [searchTerm])
 
   React.useEffect(() => {
     const sortedData = [...displayData]
     if (sortColumn !== null && sortOrder !== null) {
       const isAsc = sortOrder === "asc"
-        sortedData.sort((a, b) => {
+      sortedData.sort((a, b) => {
         const valueA = a[Object.keys(a)[sortColumn]]
         const valueB = b[Object.keys(b)[sortColumn]]
         if (isAsc) {
@@ -185,8 +210,7 @@ export const Datablify = (props) => {
     }
     setDisplayData(sortedData)
     // setCurrentPage(1)
-  }, [ sortTrigger, sortColumn, sortOrder])
-
+  }, [sortTrigger, sortColumn, sortOrder])
 
   const copyToClipboard = (e) => {
     if (timeOutId === null) {
@@ -239,7 +263,12 @@ export const Datablify = (props) => {
 
   const getRow = (row, index) => {
     return Object.entries(row).map(([key, value]) => (
-      <td key={index + Math.random()} className={styles.rowData} style={{ textAlign: "start" }} onClick={(e) => copyToClipboard(e)}>
+      <td
+        key={index + Math.random()}
+        className={styles.rowData}
+        style={{ textAlign: "start" }}
+        onClick={(e) => copyToClipboard(e)}
+      >
         {value}
       </td>
     ))
@@ -251,7 +280,8 @@ export const Datablify = (props) => {
         <h1 className={styles.errorTitle}>Error</h1>
         <p className={styles.errorText}>Categories and datas are not corresponding</p>
         <p className={styles.errorCompare}>
-          you have <span className={styles.counter}>{categories.length}</span> categories and <span className={styles.counter}>{Object.keys(data[0]).length}</span> value(s) per row{" "}
+          you have <span className={styles.counter}>{categories.length}</span> categories and{" "}
+          <span className={styles.counter}>{Object.keys(data[0]).length}</span> value(s) per row{" "}
         </p>
       </div>
     )
@@ -282,6 +312,8 @@ export const Datablify = (props) => {
     setSearchTerm(search)
   }
 
+  console.log("hello")
+
   return (
     <section className={styles.tableCompContainer} style={{ overflowX: "auto" }}>
       {isValidData ? (
@@ -294,7 +326,13 @@ export const Datablify = (props) => {
             </div>
             <Pagination nPages={nPages} currentPage={currentPage} setCurrentPage={setCurrentPage} />
             <div>
-              <input onKeyUp={(e) => searchInput(e)} onPaste={(e) => searchInput(e)} type="text" className={styles.searchInput} placeholder="Search..." />
+              <input
+                onKeyUp={(e) => searchInput(e)}
+                onPaste={(e) => searchInput(e)}
+                type="text"
+                className={styles.searchInput}
+                placeholder="Search..."
+              />
             </div>
           </div>
           <table className={styles.tableContainer}>
